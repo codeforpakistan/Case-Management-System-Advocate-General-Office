7f3eb41f4bfdb63ad96371c5903d0ca666643c5f
rehanalvi123 <94827743+rehanalvi123@users.noreply.github.com>
2022-01-20 15:03:32 +0500

    commit 04

--- application/config/database.php
+++ application/config/database.php
@@ -80,7 +80,7 @@ $db['default'] = array(
 	'password' => '',
 	'database' => 'case_management',
 	'dbdriver' => 'mysqli',
-	'port' => '3306',
+	'port' => '3308',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
