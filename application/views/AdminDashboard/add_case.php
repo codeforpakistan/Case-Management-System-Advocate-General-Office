84b1bb321ba5045e6a9793b1e48993cab032c26c
rehanalvi123 <94827743+rehanalvi123@users.noreply.github.com>
2022-01-21 12:33:33 +0500

    commit 10
    add case working.

--- application/views/AdminDashboard/add_case.php
+++ application/views/AdminDashboard/add_case.php
@@ -93,328 +93,325 @@
 
 								<div class="col-md-12">
 									<div class="form-wrap">
-										<form action="<?php echo base_url(); ?>AdminDashboard/add_cases/" method="post" class="form-horizontal">
-											<div class="form-body">
-
-												<?php if ($this->session->userdata('role') == 1 * 1) {
-												?>
-													<div class="row">
-														<div class="col-md-12">
-															<div class="form-group ">
-																<label class="control-label col-md-3">Select Branch</label>
-																<div class="col-md-6">
-																	<select required class="form-control" name="branch_id" onchange="select_cat(this);" id="branch_id">
-																		<option value="">--Select--</option>
-																		<?php foreach ($getbranches as $branch_info) { ?>
-																			<option value="<?= $branch_info->id; ?>"><?= $branch_info->title; ?></option>
-																		<?php } ?>
+<form action="<?php echo base_url(); ?>AdminDashboard/add_cases/" method="post" class="form-horizontal">
+<div class="form-body">
+
+<?php if ($this->session->userdata('role') == 1 * 1) {
+?>
+<div class="row">
+<div class="col-md-12">
+<div class="form-group ">
+<label class="control-label col-md-3">Select Branch</label>
+<div class="col-md-6">
+<select required class="form-control" name="branch_id" onChange="get_categories();" id="branch_id">
+<option value="">--Select--</option>
+<?php foreach ($getbranches as $branch_info) { ?>
+<option value="<?= $branch_info->id; ?>"><?= $branch_info->title; ?></option>
+<?php } ?>
 
-																	</select>
+</select>
 
-																	<!-- <span class="help-block">Select Role For User</span>  -->
-																</div>
-															</div>
-														</div>
+<!-- <span class="help-block">Select Role For User</span>  -->
+</div>
+</div>
+</div>
 
 
-														<!--/span-->
-													</div>
-												<?php
-												} else { ?>
-													<script>
-														window.onload = function select_cat1() {
-															var value = "<?= $this->session->userdata('branch_id') ?>";
-															if (value) {
-																$('#case_cateid').find('option').each(function() {
-																	if ($(this).attr('val') != value) {
-																		$(this).hide();
-
-																	} else {
-
-
-																		$(this).show();
-																	}
+<!--/span-->
+</div>
+<?php
+} else { ?>
+<script>
+window.onload = function select_cat1() {
+var value = "<?= $this->session->userdata('branch_id') ?>";
+if (value) {
+$('#case_cateid').find('option').each(function() {
+if ($(this).attr('val') != value) {
+$(this).hide();
 
-																});
-															}
-														}
-													</script>
-													<input type="hidden" name="branch_id" value="<?php echo $this->session->userdata('branch_id'); ?>">
-												<?php
-												}
-												?>
-
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group ">
-															<label class="control-label col-md-3">Case Category</label>
-															<div class="col-md-6">
-
-																<select required class="form-control" onchange="change_label(this);" name="case_cateid" id="case_cateid">
-																	<option id="opt" value="">--Select--</option>
-																	<?php foreach ($getCategories as $cate_info) { ?>
-																		<option val="<?= $cate_info->branch_id; ?>" value="<?= $cate_info->id; ?>"><?= $cate_info->title; ?></option>
-																	<?php } ?>
-
-																</select>
-																<!-- <span class="help-block">Select Role For User</span>  -->
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-
-
-
-
-
-
-
-
-
-												<div class="row">
-
-													<div class="col-md-12">
-														<div class="form-group">
-															<label id="case_no" class="control-label col-md-3">Case no</label>
-															<div class="col-md-3">
-																<input type="text" class="form-control" name="case_no" placeholder="">
-															</div>
-
-															<label id="case_no" class="control-label col-md-1">Year</label>
-															<div class="col-md-2">
-																<select class="form-control" name="year">
-																	<?php
-																	$year = date("Y");
-																	for ($i = $year; $i >= 1995; $i--) {
-																	?>
-																		<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-																	<?php
-																	}
-																	?>
-																</select>
-															</div>
-														</div>
-													</div>
-
-												</div>
-
+} else {
 
 
+$(this).show();
+}
 
-
-
-
-												<div class="row">
-
-													<div id="main_wp_case_no" class="col-md-12" style="display:none;">
-														<div class="form-group">
-															<label class="control-label col-md-3">WP Case no</label>
-															<div class="col-md-3">
-																<input type="text" class="form-control" name="mainWp_case_no" placeholder="">
-															</div>
+});
+}
+}
+</script>
+<input type="hidden" name="branch_id" value="<?php echo $this->session->userdata('branch_id'); ?>">
+<?php
+}
+?>
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group ">
+<label class="control-label col-md-3">Case Category</label>
+<div class="col-md-6">
+
+<select required class="form-control" onchange="change_label(this);" name="case_cateid" id="case_cateid">
+<option value="">--Select--</option>
+
+</select>
+<!-- <span class="help-block">Select Role For User</span>  -->
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
 
-															<label id="case_no" class="control-label col-md-1">Year</label>
-															<div class="col-md-2">
-																<select class="form-control" name="mainYear">
-																	<?php
-																	$year = date("Y");
-																	for ($i = $year; $i >= 1995; $i--) {
-																	?>
-																		<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-																	<?php
-																	}
-																	?>
-																</select>
-															</div>
-														</div>
-													</div>
-												</div>
 
+
+
+
+
+
+
+
+<div class="row">
+
+<div class="col-md-12">
+<div class="form-group">
+<label id="case_no" class="control-label col-md-3">Case no</label>
+<div class="col-md-3">
+<input type="text" class="form-control" name="case_no" placeholder="">
+</div>
+
+<label id="case_no" class="control-label col-md-1">Year</label>
+<div class="col-md-2">
+<select class="form-control" name="year">
+<?php
+$year = date("Y");
+for ($i = $year; $i >= 1995; $i--) {
+?>
+<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+<?php
+}
+?>
+</select>
+</div>
+</div>
+</div>
+
+</div>
 
 
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group">
-															<label class="control-label col-md-3">Title</label>
-															<div class="col-md-6">
-																<input type="text" class="form-control" name="title" placeholder="">
-																<!-- <span class="help-block"> This is inline help </span>  -->
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
 
 
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group">
-															<label class="control-label col-md-3"></label>
-															<div class="col-md-6">
-																<!--<input type="text" class="form-control" name="states" placeholder="">-->
-																VS
 
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
 
 
-
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group">
-															<label class="control-label col-md-3">State / Govt</label>
-															<div class="col-md-6">
-																<!--<input type="text" class="form-control" name="states" placeholder="">-->
+<div class="row">
 
-																<input type="radio" name="stateorgovt" value="Government">&nbsp;&nbsp;&nbsp;Government &nbsp;&nbsp;&nbsp;
-																<input type="radio" name="stateorgovt" value="State">&nbsp;&nbsp;&nbsp;State
+<div id="main_wp_case_no" class="col-md-12" style="display:none;">
+<div class="form-group">
+<label class="control-label col-md-3">WP Case no</label>
+<div class="col-md-3">
+<input type="text" class="form-control" name="mainWp_case_no" placeholder="">
+</div>
 
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-
-												<!-- /Row -->
-
-												<!-- /Row -->
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group">
-															<label class="control-label col-md-3">Advocate For Petitioner</label>
-															<div class="col-md-6">
-																<input type="text" class="form-control" name="petitioner_advocate" placeholder="">
-																<!-- <span class="help-block"> This is inline help </span>  -->
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-
-
-
-												<!-- /Row -->
-
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group ">
-															<label class="control-label col-md-3">Respondent(s)</label>
-															<div class="col-md-6">
-																<input type="text" class="form-control" name="respondent" placeholder="">
-
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-
-
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group ">
-															<label class="control-label col-md-3">Advocate for Respondent(s)</label>
-															<div class="col-md-6">
-																<input type="text" class="form-control" name="advocate_respondent" placeholder="">
-
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-
-
-
-
-
-
-												<!-- /Row -->
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group">
-															<label class="control-label col-md-3">Department</label>
-															<div class="col-md-6">
-
-																<!--<select class="form-control multi-select" name="department_id">-->
-
-																<select class="form-control select2 select2-multiple select2-container--focus select2-container--open" name="department_id[]" multiple="multiple">
-
-																	<?php foreach ($getdepartments as $department_info) { ?>
-																		<option value="<?= $department_info->id; ?>"><?= $department_info->title; ?></option>
-																	<?php } ?>
-																</select>
-																<!-- <span class="help-block">Select Role For User</span>  -->
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-
-
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group">
-															<label class="control-label col-md-3">Focal Person</label>
-															<div class="col-md-6">
-																<input type="text" class="form-control" name="focalperson" placeholder="">
-																<!-- <span class="help-block"> This is inline help </span>  -->
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-												</div>
-
-
-
-												<div class="row">
-													<div class="col-md-12">
-														<div class="form-group">
-															<label class="control-label col-md-3">Court</label>
-															<div class="col-md-6">
-																<select class="form-control" name="court_id">
-																	<?php foreach ($getcourts as $court_info) { ?>
-																		<option value="<?= $court_info->id; ?>"><?= $court_info->title; ?></option>
-																	<?php } ?>
-
-																</select>
-																<!-- <span class="help-block">Select Role For User</span>  -->
-															</div>
-														</div>
-													</div>
-													<!--/span-->
-
-
-													<div class="row">
-														<div class="col-md-12">
-															<div class="form-group">
-																<label class="control-label col-md-3">Case Filing Date</label>
-																<div class="col-md-6">
-																	<input type="date" class="form-control" name="Filling_date" value="<?php echo date("Y-m-d"); ?>" placeholder="">
-																	<!-- <span class="help-block"> This is inline help </span>  -->
-																</div>
-															</div>
-														</div>
-														<!--/span-->
-													</div>
-
-												</div>
-
-												<div class="col-md-12">
-													<div class="form-group ">
-														<label class="control-label col-md-3">Government is Petitioner</label>
-														<div class="col-md-6">
-															<input type="checkbox" name="government_petitioner" value="Yes">
-
-														</div>
-													</div>
-												</div>
-
-
-
-												<!--<div  class="col-md-12">
+<label id="case_no" class="control-label col-md-1">Year</label>
+<div class="col-md-2">
+<select class="form-control" name="mainYear">
+<?php
+$year = date("Y");
+for ($i = $year; $i >= 1995; $i--) {
+?>
+<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
+<?php
+}
+?>
+</select>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<label class="control-label col-md-3">Title</label>
+<div class="col-md-6">
+<input type="text" class="form-control" name="title" placeholder="">
+<!-- <span class="help-block"> This is inline help </span>  -->
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<label class="control-label col-md-3"></label>
+<div class="col-md-6">
+<!--<input type="text" class="form-control" name="states" placeholder="">-->
+VS
+
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<label class="control-label col-md-3">State / Govt</label>
+<div class="col-md-6">
+<!--<input type="text" class="form-control" name="states" placeholder="">-->
+
+<input type="radio" name="stateorgovt" value="Government">&nbsp;&nbsp;&nbsp;Government &nbsp;&nbsp;&nbsp;
+<input type="radio" name="stateorgovt" value="State">&nbsp;&nbsp;&nbsp;State
+
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+<!-- /Row -->
+
+<!-- /Row -->
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<label class="control-label col-md-3">Advocate For Petitioner</label>
+<div class="col-md-6">
+<input type="text" class="form-control" name="petitioner_advocate" placeholder="">
+<!-- <span class="help-block"> This is inline help </span>  -->
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+
+
+<!-- /Row -->
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group ">
+<label class="control-label col-md-3">Respondent(s)</label>
+<div class="col-md-6">
+<input type="text" class="form-control" name="respondent" placeholder="">
+
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group ">
+<label class="control-label col-md-3">Advocate for Respondent(s)</label>
+<div class="col-md-6">
+<input type="text" class="form-control" name="advocate_respondent" placeholder="">
+
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+
+
+
+
+
+<!-- /Row -->
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<label class="control-label col-md-3">Department</label>
+<div class="col-md-6">
+
+<!--<select class="form-control multi-select" name="department_id">-->
+
+<select class="form-control select2 select2-multiple select2-container--focus select2-container--open" name="department_id[]" multiple="multiple">
+
+<?php foreach ($getdepartments as $department_info) { ?>
+<option value="<?= $department_info->id; ?>"><?= $department_info->title; ?></option>
+<?php } ?>
+</select>
+<!-- <span class="help-block">Select Role For User</span>  -->
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<label class="control-label col-md-3">Focal Person</label>
+<div class="col-md-6">
+<input type="text" class="form-control" name="focalperson" placeholder="">
+<!-- <span class="help-block"> This is inline help </span>  -->
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<label class="control-label col-md-3">Court</label>
+<div class="col-md-6">
+<select class="form-control" name="court_id">
+<?php foreach ($getcourts as $court_info) { ?>
+<option value="<?= $court_info->id; ?>"><?= $court_info->title; ?></option>
+<?php } ?>
+
+</select>
+<!-- <span class="help-block">Select Role For User</span>  -->
+</div>
+</div>
+</div>
+<!--/span-->
+
+
+<div class="row">
+<div class="col-md-12">
+<div class="form-group">
+<label class="control-label col-md-3">Case Filing Date</label>
+<div class="col-md-6">
+<input type="date" class="form-control" name="Filling_date" value="<?php echo date("Y-m-d"); ?>" placeholder="">
+<!-- <span class="help-block"> This is inline help </span>  -->
+</div>
+</div>
+</div>
+<!--/span-->
+</div>
+
+</div>
+
+<div class="col-md-12">
+<div class="form-group ">
+<label class="control-label col-md-3">Government is Petitioner</label>
+<div class="col-md-6">
+<input type="checkbox" name="government_petitioner" value="Yes">
+
+</div>
+</div>
+</div>
+
+
+
+<!--<div  class="col-md-12">
 <div id="link_case" class="form-group">
 <label class="control-label col-md-3">Previous case</label>
 <div class="col-md-6">
@@ -432,19 +429,19 @@ foreach ($getCases as $case_info) { ?>
 </div>-->
 
 
-												<div class="form-actions mt-10">
-													<div class="row">
-														<div class="col-md-12">
-															<div class="row">
-																<div class="col-md-offset-3 col-md-9">
-																	<button type="submit" class="btn btn-success  mr-10">Submit</button>
-																</div>
-															</div>
-														</div>
-														<div class="col-md-6"> </div>
-													</div>
-												</div>
-										</form>
+<div class="form-actions mt-10">
+<div class="row">
+<div class="col-md-12">
+<div class="row">
+<div class="col-md-offset-3 col-md-9">
+<button type="submit" class="btn btn-success  mr-10">Submit</button>
+</div>
+</div>
+</div>
+<div class="col-md-6"> </div>
+</div>
+</div>
+</form>
 									</div>
 								</div>
 							</div>
@@ -479,24 +476,31 @@ foreach ($getCases as $case_info) { ?>
 
 
 
-		function select_cat(elem) {
-			var value = elem.value;
-			if (value) {
-
-
-				$('#case_cateid').find('option').each(function() {
-					if ($(this).attr('val') != value) {
-						$(this).hide();
-					} else {
-
-
-						$(this).show();
-					}
-
-				});
-			}
-
-
-
+		function get_categories()
+		{
+		
+		var branch_id = $("#branch_id").val();
+		
+		if(branch_id != '')
+		{   
+		$.ajax({
+		url:"<?php echo base_url(); ?>AdminDashboard/fetch_caseCategories",
+		method:"POST",
+		beforeSend: function() 
+		{
+		$("#userstatus").html('<img src="<?php echo base_url(); ?>/assets/images/loading.gif">');
+		},
+		data:{branch_id : branch_id},
+		success:function(data)
+		{
+		////$("#userstatus").fadeOut(); 
+		$('#case_cateid').html(data);
+		}
+		});
+		}
+		else
+		{
+		$("#userstatus").text('Please Select District.');   
+		}
 		}
 	</script>
